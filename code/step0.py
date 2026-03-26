0dc20ea11eae27b04955bbeb26a3a4814a496afa
twitterbotf1 <twitterbotf1@gmail.com>
2026-03-26 20:34:10 +0530

    json

--- code/step0.py
+++ code/step0.py
@@ -1,17 +1,10 @@
 import os
 
-def clean_directory():
-    directory = "/workspaces/twitterbotscraper/code"
-    whitelist = {
-        "key.txt", "sources.txt", "step0.py", "step1.py", 
-        "step2.py", "step3.py", "step4.py", "step5.py"
-    }
-
+def clean_json():
+    directory = "code"
     for filename in os.listdir(directory):
-        if filename not in whitelist:
-            file_path = os.path.join(directory, filename)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
+        if filename.endswith(".json"):
+            os.remove(os.path.join(directory, filename))
 
 if __name__ == "__main__":
-    clean_directory()
+    clean_json()
